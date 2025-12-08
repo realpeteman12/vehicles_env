ea4965c2b9b6ee5592bdab892d33ac6220af82c5
Peteman <realpeteman12@gmail.com>
2025-12-07 19:17:57 -0600

    Agregar keys únicos a los botones para evitar error de IDs duplicados

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ car_data = pd.read_csv('vehicles_us.csv')
 
 st.header('Análisis de Datos de Vehículos')
 
-hist_button = st.button('Construir histograma')
+hist_button = st.button('Construir histograma', key='hist_btn')
 
 if hist_button:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
@@ -21,7 +21,7 @@ if scatter_button:
     fig = px.scatter(car_data, x="odometer", y="price")
     st.plotly_chart(fig, use_container_width=True)
 
-scatter_button = st.button('Construir gráfico de dispersión')
+scatter_button = st.button('Construir gráfico de dispersión', key='scatter_btn')
 
 if scatter_button:
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
